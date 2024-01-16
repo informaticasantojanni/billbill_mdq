ca1aade8587da1f2afbc978b4ed20a6dfc92a5f6
titojp <titojp2002@hotmail.com>
2024-01-16 10:51:40 -0300

    jp commit

--- src/components/pageHeader/PageHeader.jsx
+++ src/components/pageHeader/PageHeader.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./PageHeader.css"
 import "../../styles.css"
+import olita from "../../assets/olita.svg"
 
 const PageHeader = ({ title, image }) => {
     return (
@@ -11,12 +12,12 @@ const PageHeader = ({ title, image }) => {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.1)), url(${image})`,
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)), url(${image})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
         }}>
-            <div className="header--container">
-                <h2 className='title1Size1 fontStyle1'>{title}</h2>
+            <div className="header-container">
+                <h2 className='title1Size1 fontStyle1 fontColor1'>{title}</h2>
             </div>
         </div>
     )
