27a482780284885d87352441111dee20f84bffcf
titojp <titojp2002@hotmail.com>
2024-01-17 15:32:09 -0300

    added scroll smooth

--- src/pages/rooms/view/RoomsView.jsx
+++ src/pages/rooms/view/RoomsView.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import "./roomsView.css"
 import PageHeader from '../../../components/pageHeader/PageHeader';
 import image from '../../../../src/assets/gallery_mdq/billbillMdq3.jpg';
@@ -8,6 +8,10 @@ import RoomsList from '../components/RoomsList';
 const RoomsView = () => {
 
   const { t } = useRooms();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
   return (
     <div className='Rooms'>
